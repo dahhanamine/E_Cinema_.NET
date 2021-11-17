dab906ca441a332833810a8be52535fb20324508
Amine <aminedahhan07@gmail.com>
2021-11-17 13:20:01 +0100

    firstCommit_Models

---
+++ E_Cinema/Data/MovieCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Cinema.Data
+{
+    public enum MovieCategory
+    {
+
+        Action = 1 , 
+        Drama , 
+        Documentary ,
+        Romantique ,
+        Comedy 
+    }
+}
---
+++ E_Cinema/Models/Movies.cs
@@ -0,0 +1,27 @@
+﻿using E_Cinema.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Cinema.Models
+{
+    public class Movies
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        public double Price { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
