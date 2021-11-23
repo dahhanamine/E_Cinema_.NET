7576a21efb4be5b9a5164e12867fcadd278422a6
Amine <aminedahhan07@gmail.com>
2021-11-23 23:40:40 +0100

    newRepository_2

--- E_Cinema/Data/Services/IActorsService.cs
+++ E_Cinema/Data/Services/IActorsService.cs
@@ -9,15 +9,7 @@ namespace E_Cinema.Data.Services
     public interface IActorsService
     {
 
-        Task<IEnumerable<Actor>> GetAllAsync();
-
-        Task<Actor> GetByIdAsync(int id);
-
-        Task AddAsync(Actor actor);
-
-        Task<Actor> UpdateAsync(int id, Actor newActor);
-
-        Task DeleteAsync(int id);
+       
 
     }
 }
